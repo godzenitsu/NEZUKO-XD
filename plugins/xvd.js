dc28b52ace52074215f37d884bde12aa4efac700
godzenitsu <164893015+godzenitsu@users.noreply.github.com>
2024-03-30 10:41:14 +0530

    𝗘𝗗𝗜𝗧𝗘𝗗 𝗕𝗬 𝗭𝗘𝗡𝗜𝗧𝗦𝗨

--- plugins/xvd.js
+++ plugins/xvd.js
@@ -14,13 +14,13 @@ var sex = await fetch(`https://raganork-network.vercel.app/api/xvideos/download?
         var fek = await sex.json();
 
         await message.client.sendMessage(message.jid,{video:{ url: fek.url}, contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
+title: "𝗤𝗨𝗘𝗘𝗡 𝗡𝗘𝗭𝗨𝗞𝗢",
 body: "𝙑𝙞𝙙𝙚𝙤 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙚𝙙 𝙎𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮",
 sourceUrl: "",
 mediaUrl: "",
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption : (config.CAPTION)}, {quoted : message})
+thumbnailUrl: "https://i.imgur.com/PRKcWJ0.jpeg" }}, caption : (config.CAPTION)}, {quoted : message})
     }
     );
