01bc178776df8a5aa11f79cb0049a1364355a630
godzenitsu <164893015+godzenitsu@users.noreply.github.com>
2024-03-30 10:23:56 +0530

    Update emix.js

--- plugins/emix.js
+++ plugins/emix.js
@@ -8,21 +8,21 @@ command(
         type: "converter",
     },
     async (message, match) => {
-        if (!match) return await message.sendMessage("*_Need emojis_*\n*_eg:- emix 🙄😌_*");
+        if (!match) return await message.sendMessage("*_emojis tha myre_*\n*_eg:- emix 🙄😌_*");
 try {
         var { result } = await getJson(`https://levanter.onrender.com/emix?q=${match}`);
         let buff = await getBuffer(result);
         await message.sendMessage(
       buff,
       { packname: config.STICKER_DATA.split(";")[0], author: config.STICKER_DATA.split(";")[1], contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
+title: "𝗤𝗨𝗘𝗘𝗡 𝗡𝗘𝗭𝗨𝗞𝗢",
 body: "𝙚𝙢𝙤𝙟𝙞 𝙢𝙞𝙭𝙚𝙙",
 sourceUrl: "",
 mediaUrl: "",
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }} },
+thumbnailUrl: "https://i.imgur.com/PRKcWJ0.jpeg" }} },
       "sticker"
     );
     } catch (error) {
