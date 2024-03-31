edd9bbb9d7c891dd2e98b6b7177eb12b783d62ec
godzenitsu <164893015+godzenitsu@users.noreply.github.com>
2024-03-31 09:46:18 +0530

    Update mediafire.js

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -24,7 +24,7 @@ response.push({ name, mime, size, link })
 return response
 }
 var zeta = await mediafiredownload(`${match}`)
-await message.client.sendMessage(message.jid, { text: `*_Downloading ${zeta[0]['name']}_*\n\n*size : ${zeta[0]['size']}*\n\n𝐄𝐙𝐑𝐀-𝐗𝐃` },{ quoted: message});
+await message.client.sendMessage(message.jid, { text: `*_Downloading ${zeta[0]['name']}_*\n\n*size : ${zeta[0]['size']}*\n\n𝗤𝗨𝗘𝗘𝗡 𝗡𝗘𝗭𝗨𝗞𝗢` },{ quoted: message});
 await message.client.sendMessage(message.jid, { document :{ url: zeta[0]['link'] }, fileName: zeta[0]['name'] , mimetype: zeta[0]['mime'], contextInfo: { externalAdReply: {
 title: "𝗤𝗨𝗘𝗘𝗡 𝗡𝗘𝗭𝗨𝗞𝗢",
 body: "𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙚𝙙 𝙎𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮",
@@ -33,6 +33,6 @@ mediaUrl: "",
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/PRKcWJ0.jpeg" }} }, {quoted: message })
+thumbnailUrl: "https://i.imgur.com/sPBOwto.jpeg" }} }, {quoted: message })
 }
 );
