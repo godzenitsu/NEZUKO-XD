5928f181ac103e5bb08ab054834791c478609f7e
godzenitsu <164893015+godzenitsu@users.noreply.github.com>
2024-04-01 15:37:41 +0530

    𝗤𝗨𝗘𝗘𝗡 𝗡𝗘𝗭𝗨𝗞𝗢

--- plugins/xvs.js
+++ plugins/xvs.js
@@ -28,6 +28,6 @@ mediaUrl: "",
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/PRKcWJ0.jpeg" }}, caption: (txt)}, {quoted: message })
+thumbnailUrl: "https://i.imgur.com/xZ45hxR.jpeg" }}, caption: (txt)}, {quoted: message })
     }
     );
